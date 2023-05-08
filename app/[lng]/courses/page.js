711b116cb7c014f61fff21bb804b26b29b40213c
devbisry <127805608+devbisry@users.noreply.github.com>
2023-05-08 06:22:23 +0000

    :sparkles: 2nd UI design

--- app/[lng]/courses/page.js
+++
@@ -1,7 +0,0 @@
-export default function Courses() {
-  return (
-    <>
-      <h1> Courses are Here </h1>
-    </>
-  );
-}
--- next.config.js
+++ next.config.js
@@ -1,33 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
-  swcMinify: true,
-  experimental: {
-    // Required:
-    appDir: true
+module.exports = {
+  swcMinify: false,
+  trailingSlash: true,
+  env: {
+    // HOST
+    HOST_API_KEY: 'https://api-dev-minimal-v4.vercel.app',
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "addisababa.eotc.org.et",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "www.pngitem.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "source.unsplash.com",
-        port: "",
-        pathname: "/**",
-      }
-    ],
-  },
-}
-
-module.exports = nextConfig
+};
